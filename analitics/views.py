ce201da808ee0700bafcfec15d120a60d11b5f1f
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-23 14:04:34 +0330

    add standard deviations in view file into dashboard page

--- analitics/views.py
+++ analitics/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
+
 from analitics.descriptive.totals import record_users, record_ratings, record_movies, record_tags
 from analitics.descriptive.nuniques import unique_tag, unique_movie, unique_rating, unique_user
 from analitics.descriptive.means import mean_users, mean_movies, mean_ratings, mean_tags
+from analitics.descriptive.standard_deviation import std_user, std_tags, std_movies, std_rating
 
 
 def home():
@@ -25,4 +27,9 @@ def dashboard():
         mean_movies=mean_movies,
         mean_ratings=mean_ratings,
         mean_tags=mean_tags,
+        # ________________________________
+        std_user=std_user,
+        std_movies=std_movies,
+        std_rating=std_rating,
+        std_tags=std_tags,
     )
