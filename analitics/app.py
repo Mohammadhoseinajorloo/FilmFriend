78cb1ab634c3a1a672b97d9f10fc3c163d85c538
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-07-23 14:45:24 +0330

    fix import view page in app file

--- analitics/app.py
+++ analitics/app.py
@@ -1,12 +1,13 @@
-import views
 from flask import Flask
 
+from analitics.views import home, dashboard
 from analitics.core.config import settings
+
 
 app = Flask(settings.APP_NAME)
 
-app.add_url_rule("/", methods=["GET"], view_func=views.home)
-app.add_url_rule("/dashboard", methods=["GET"], view_func=views.dashboard)
+app.add_url_rule("/", methods=["GET"], view_func=home)
+app.add_url_rule("/dashboard", methods=["GET"], view_func=dashboard)
 
 if __name__ == "__main__":
     print(f"loading analitic website {app.name} ...")
